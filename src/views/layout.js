7a8d672f3cd6bdf33d02c7ec53b3388899141afe
Owen Casabuena <owencasabuena@gmail.com>
2026-04-06 13:56:55 +0800

    feat(views): implement layout.js

---
+++ src/views/layout.js
@@ -0,0 +1,19 @@
+import { he } from "date-fns/locale";
+
+export function renderLayout() {
+    const content = document.querySelector('#content');
+
+    const header = document.createElement('header');
+    const sidebar = document.createElement('aside');
+    const main = document.createElement('main');
+    
+    header.id = 'header';
+    sidebar.id = 'sidebar-container';
+    main.id = 'tasks-container';
+
+    header.classList.add('header');
+    sidebar.classList.add('sidebar');
+    main.classList.add('main');
+
+    content.append(header, sidebar, main);
+}
